530d0ed5d6967f3d2f5df128001727a62490c4ed
Pavlo Bondarenko <bondarenkopavloua@yahoo.com>
2024-08-05 11:25:15 -0500

    v1.1 stable - fixed followers.txt

--- bot.py
+++ bot.py
@@ -11,17 +11,17 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 GITHUB_USER = "OfficialCodeVoyage"
 PERSONAL_GITHUB_TOKEN = os.getenv('personal_github_token')
 FOLLOWER_URL = f'https://api.github.com/users/{GITHUB_USER}/followers?page='
+FOLLOWING_URL = f'https://api.github.com/users/{GITHUB_USER}/following?page='
 UPDATE_FOLLOWED_USER = 'https://api.github.com/user/following/{}'
 
 
-
-def fetch_followers(page):
+def fetch_users(url, page):
     try:
-        response = requests.get(FOLLOWER_URL + str(page))
+        response = requests.get(url + str(page))
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.json()
     except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching followers: {e}")
+        logging.error(f"Error fetching users from {url}: {e}")
         return []
 
 
@@ -62,14 +62,15 @@ def save_follower_count(file_path, count):
 
 
 def main():
-    logging.info('Starting to fetch your follower lists...')
+    logging.info('Starting to fetch your follower and following lists...')
 
     follower_counter = 0
     page = 1
     followed_users = read_followed_users('./followers.txt')
 
+    # Fetch followers
     while True:
-        follower_lists = fetch_followers(page)
+        follower_lists = fetch_users(FOLLOWER_URL, page)
         if not follower_lists:
             break
 
@@ -84,6 +85,28 @@ def main():
 
         page += 1
         time.sleep(3)
+
+    # Fetch following
+    page = 1
+    following_users = set()
+    while True:
+        following_lists = fetch_users(FOLLOWING_URL, page)
+        if not following_lists:
+            break
+
+        for following_info in following_lists:
+            user = following_info['login']
+            following_users.add(user)
+
+        page += 1
+        time.sleep(3)
+
+    # Compare and reconcile
+    not_followed_back = following_users.difference(followed_users)
+    if not_followed_back:
+        logging.info(f"Users you follow but are not in followers.txt: {', '.join(not_followed_back)}")
+    else:
+        logging.info("All users you follow are listed in followers.txt.")
 
     save_follower_count('follower_counter.txt', follower_counter)
     logging.info(f'Following users from your followers list is done!')
